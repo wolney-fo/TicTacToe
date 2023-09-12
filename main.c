8735de41896e1acf511541ce6c42bc0c1a11f237
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2023-09-11 23:47:59 -0300

    apply clean code 🧹

--- main.c
+++ main.c
@@ -2,122 +2,128 @@
 #include <conio.h>
 #include <stdlib.h>
 
-int verifyWin();
-int verifyDraw();
-void printBoard();
+#define MAX_MOVES 9
+#define BOARD_SIZE 3
+
+void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE]);
+void printBoard(const char board[BOARD_SIZE][BOARD_SIZE]);
+void makeMove(char board[BOARD_SIZE][BOARD_SIZE], int move, char currentPlayer);
+int isWin(const char board[BOARD_SIZE][BOARD_SIZE]);
+int isDraw(const char board[BOARD_SIZE][BOARD_SIZE]);
+int isValidMove(const char board[BOARD_SIZE][BOARD_SIZE], int move);
 
 int main()
 {
-    char board[3][3] = {
-        {'_', '_', '_'},
-        {'_', '_', '_'},
-        {'_', '_', '_'}
-    };
+    char board[BOARD_SIZE][BOARD_SIZE];
+    initializeBoard(board);
 
-    char turn = 'X';
+    char currentPlayer = 'X';
+    int move;
 
+    // Main menu
     system("cls");
-
-    puts("XOXOXOXOXOXOXOXOXOXOX");
-    puts("|||| Tic-Tac-Toe ||||");
-    puts("XOXOXOXOXOXOXOXOXOXOX");
-    puts("");
-    puts("How to play: choose any empty spot of board on your turn by its number");
-    printf(" 1 | 2 | 3\n-----------\n 4 | 5 | 6\n-----------\n 7 | 8 | 9\n");
-    puts("You ready? Press any key to start!!");
+    printf("XOXOXOXOXOXOXOXOXOXOX\n|||| Tic-Tac-Toe ||||\nXOXOXOXOXOXOXOXOXOXOX\n\n");
+    printf("How to play: choose any empty spot on the board by its number (1-9).\n");
+    printf("Game map:\n 1 | 2 | 3\n-----------\n 4 | 5 | 6\n-----------\n 7 | 8 | 9\n");
+    printf("You ready? Press any key to start!!\n");
     getch();
 
-    do {
-        int move;
+    for (int currentMove = 0; currentMove < MAX_MOVES; currentMove++) {
         system("cls");
         printBoard(board);
 
-        printf("\n[%c] > ", turn);
-        scanf_s("%d", &move);
+        do {
+            printf("\n[%c] > ", currentPlayer);
+            scanf_s("%d", &move);
+        } while (!isValidMove(board, move));
 
-        switch (move) {
-            case 1:
-                board[0][0] = turn;
-                break;
-            case 2:
-                board[0][1] = turn;
-                break;
-            case 3:
-                board[0][2] = turn;
-                break;
-            case 4:
-                board[1][0] = turn;
-                break;
-            case 5:
-                board[1][1] = turn;
-                break;
-            case 6:
-                board[1][2] = turn;
-                break;
-            case 7:
-                board[2][0] = turn;
-                break;
-            case 8:
-                board[2][1] = turn;
-                break;
-            case 9:
-                board[2][2] = turn;
-                break;
-            default:
-                printf("\n\nInvalid position!!");
+        makeMove(board, move, currentPlayer);
+
+        if (isWin(board) == 1) {
+            system("cls");
+            printf("Player %c wins!\n\n", currentPlayer);
+            printBoard(board);
+            break;
+        } else if (isDraw(board) == 1) {
+            system("cls");
+            printf("It's a draw!\n\n");
+            printBoard(board);
+            break;
         }
 
-        turn = (turn == 'X') ? 'O' : 'X';
-
-    } while (verifyWin(board) == 0 && verifyDraw(board) == 0) ;
-
-    system("cls");
-    printf("Finished game!!!\n");
-    printBoard(board);
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
 
     return 0;
 }
 
-int verifyWin(char currentBoard[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        if ( (currentBoard[i][0] == currentBoard[i][1] && currentBoard[i][1] == currentBoard[i][2]) && (currentBoard[i][0] == 'X' || currentBoard[i][0] == 'O') ) {
+int isWin(const char board[BOARD_SIZE][BOARD_SIZE]) {
+    // Horizontal
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        if ( (board[i][0] == board[i][1] && board[i][1] == board[i][2]) && (board[i][0] == 'X' || board[i][0] == 'O') ) {
             return 1;
         }
 
     }
 
-    for (int j = 0; j < 3; j++) {
-        if ( (currentBoard[0][j] == currentBoard[1][j] && currentBoard[1][j] == currentBoard[2][j]) && (currentBoard[0][j] == 'X' || currentBoard[0][j] == 'O') ) {
+    // Vertical
+    for (int j = 0; j < BOARD_SIZE; j++) {
+        if ( (board[0][j] == board[1][j] && board[1][j] == board[2][j]) && (board[0][j] == 'X' || board[0][j] == 'O') ) {
             return 1;
         }
     }
 
-    if ( (currentBoard[0][0] == currentBoard[1][1] && currentBoard[0][0] == currentBoard[2][2]) && (currentBoard[0][0] == 'X' || currentBoard[0][0] == 'O') ) {
+    // Diagonal
+    if ( (board[0][0] == board[1][1] && board[0][0] == board[2][2]) && (board[0][0] == 'X' || board[0][0] == 'O') ) {
         return 1;
     }
 
-    if ( (currentBoard[2][0] == currentBoard[1][1] && currentBoard[1][1] == currentBoard[0][2]) && (currentBoard[2][0] == 'X' || currentBoard[2][0] == 'O') ) {
+    if ( (board[2][0] == board[1][1] && board[1][1] == board[0][2]) && (board[2][0] == 'X' || board[2][0] == 'O') ) {
         return 1;
     }
 
     return 0;
 }
 
-int verifyDraw(char currentBoard[3][3]) {
-    for (int line = 0; line < 3; line++) {
-        if (currentBoard[line][0] == '_' || currentBoard[line][1] == '_' || currentBoard[line][2]) {
+int isDraw(const char board[BOARD_SIZE][BOARD_SIZE]) {
+    for (int line = 0; line < BOARD_SIZE; line++) {
+        if (board[line][0] == '_' || board[line][1] == '_' || board[line][2] == '_') {
             return 0;
         }
     }
     return 1;
 }
 
-void printBoard(char currentBoard[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        printf(" %c | %c | %c \n", currentBoard[i][0], currentBoard[i][1], currentBoard[i][2]);
-        if (i < 2) {
-            printf("-----------");
-            printf("\n");
+int isValidMove(const char board[BOARD_SIZE][BOARD_SIZE], int move) {
+    if (move >= 1 && move <= 9) {
+        int row = (move - 1) / 3;
+        int col = (move - 1) % 3;
+        if (board[row][col] == '_') {
+            return 1;
         }
+    }
+    return 0;
+}
+
+void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE]) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            board[i][j] = '_';
+        }
+    }
+}
+
+void printBoard(const char board[BOARD_SIZE][BOARD_SIZE]) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+        if (i < BOARD_SIZE - 1) printf("-----------\n");
+    }
+}
+
+void makeMove(char board[BOARD_SIZE][BOARD_SIZE], int move, char currentPlayer) {
+    if (move >= 1 && move <= 9) {
+        int row = (move - 1) / 3;
+        int col = (move - 1) % 3;
+        board[row][col] = currentPlayer;
     }
 }
